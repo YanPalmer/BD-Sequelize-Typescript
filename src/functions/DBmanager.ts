4a98a4a1bc8cde13a15e9e2a509c35e8dbb7973d
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-09-10 22:28:26 -0300

    Alguns ajustes
    Criei uma função para reutilizar a function buscarUsuario()

--- src/functions/DBmanager.ts
+++ src/functions/DBmanager.ts
@@ -28,23 +28,24 @@ export async function DBmanager(execucao: number) {
     async function procurarDados() {
         console.log("Procurar dados...");
         const dadoEmail = String(prompt("Digite o Email: "));
-        const buscarUsuario = async (email: string) => {
-            try {
-                const usuario = await User.findOne({
-                    where: {
-                        email: dadoEmail,
-                    },
-                });
-
-                if (usuario) {
-                    console.log(`Resultado encontrado:`);
-                    console.log(`ID: ${usuario.dataValues.id}\nNome: ${usuario.dataValues.name}\nEmail: ${usuario.dataValues.email}`);
-                }
-            } catch (error) {
-                console.error("Ocorreu um erro ao buscar o usuário:", error);
-            }
-        }
         await buscarUsuario(dadoEmail);
+        // const buscarUsuario = async (email: string) => {
+        //     try {
+        //         const usuario = await User.findOne({
+        //             where: {
+        //                 email: dadoEmail,
+        //             },
+        //         });
+
+        //         if (usuario) {
+        //             console.log(`Resultado encontrado:`);
+        //             console.log(`ID: ${usuario.dataValues.id}\nNome: ${usuario.dataValues.name}\nEmail: ${usuario.dataValues.email}`);
+        //         }
+        //     } catch (error) {
+        //         console.error("Ocorreu um erro ao buscar o usuário:", error);
+        //     }
+        // }
+        // await buscarUsuario(dadoEmail);
         // User.findOne({
         //     where: {
         //         name: dadoNome,
@@ -57,7 +58,7 @@ export async function DBmanager(execucao: number) {
     }
 
     async function excluirDados() {
-        User.drop();
+        let email: string = prompt("Email a ser excluído: ");
         console.log(`Excluido com sucesso...`)
         reinicializar();
     }
@@ -95,4 +96,27 @@ function reinicializar() {
     setTimeout(() => {
         inicializar();
     }, 4000);
+}
+
+const buscarUsuario = async (dadoEmail: string) => {
+    try {
+        const usuario = await User.findOne({
+            where: {
+                email: dadoEmail,
+            },
+        });
+
+        if (!usuario) {
+            throw new Error("Usuário não encontrado");
+        }
+        console.log(`Resultado encontrado:
+${"=".repeat(30)}
+ID: ${usuario.dataValues.id}
+Nome: ${usuario.dataValues.name}
+Email: ${usuario.dataValues.email}
+${"=".repeat(30)}`);
+        // console.log(`ID: ${usuario.dataValues.id}\nNome: ${usuario.dataValues.name}\nEmail: ${usuario.dataValues.email}`);
+    } catch (error) {
+        console.error(error);
+    }
 }
